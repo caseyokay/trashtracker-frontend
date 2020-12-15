dc5e0dc4399741aec523eff2432658d798fbd02d
github username <caseyoksana@gmail.com>
2020-12-15 14:06:38 -0500

    Started setting up welcome page and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Welcome from './Components/Welcome';
 
 class App extends React.Component{
   render(){
     return(
-      <h1>Hey its an App</h1>
+      <>
+      <Switch>
+
+      <Route path="/welcome" render={()=> <Welcome/>} />
+      <h1>App.js Checkkk</h1>
+      </Switch>
+      </>
     );
   }
 };
---
+++ src/Components/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Welcome(){
+    return <p>Welcome to the Trash App</p>
+}
+
+export default Welcome;
