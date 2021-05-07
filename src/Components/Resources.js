fc8f0a8e110851697efe06bbad9c20f7d160ab01
github username <caseyoksana@gmail.com>
2021-05-07 14:05:04 -0400

    new page

---
+++ src/Components/Resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Resources extends React.Component {
+    render(){
+        return(
+            <div className="resources">
+            <p>
+                More information and other ways you can take action:
+            </p>
+            </div>
+        )
+    }
+}
+
+export default Resources;
